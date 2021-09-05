fe8fa475d6666938ea596891f5d5f6b017ac0261
dioums21 <dioums21@users.noreply.github.com>
2021-09-04 21:59:24 -0400

    profile display, excluding repos display

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -31,14 +31,14 @@ const Login = ({ login, isAuthenticated }) => {
       <p className='lead'>
         <i className='fas fa-user'></i> Sign Into Your Account
       </p>
-      <form className='form' onSubmit={e => onSubmit(e)}>
+      <form className='form' onSubmit={onSubmit}>
         <div className='form-group'>
           <input
             type='email'
             placeholder='Email Address'
             name='email'
             value={email}
-            onChange={e => onChange(e)}
+            onChange={onChange}
             required
           />
         </div>
@@ -49,7 +49,7 @@ const Login = ({ login, isAuthenticated }) => {
             name='password'
             minLength='6'
             value={password}
-            onChange={e => onChange(e)}
+            onChange={onChange}
           />
         </div>
 
